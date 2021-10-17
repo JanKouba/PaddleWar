02d557dbb2b15aa800041f4cf1e88bc817a81f21
Ferda Mravenec <jan.kouba@2u2.cz>
2021-10-17 16:57:32 +0200

    Přidejte soubory projektu.

---
+++ PaddleWar/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace PaddleWar
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblStart = new System.Windows.Forms.Label();
+            this.lblGameOver = new System.Windows.Forms.Label();
+            this.picGameOver = new System.Windows.Forms.PictureBox();
+            this.picPaddle = new System.Windows.Forms.PictureBox();
+            this.lblBall = new System.Windows.Forms.Label();
+            this.lblBounce = new System.Windows.Forms.Label();
+            this.picBall = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pnlMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picGameOver)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPaddle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pnlMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlMain.Controls.Add(this.lblScore);
+            this.pnlMain.Controls.Add(this.lblStart);
+            this.pnlMain.Controls.Add(this.lblGameOver);
+            this.pnlMain.Controls.Add(this.picGameOver);
+            this.pnlMain.Controls.Add(this.picPaddle);
+            this.pnlMain.Controls.Add(this.lblBall);
+            this.pnlMain.Controls.Add(this.lblBounce);
+            this.pnlMain.Controls.Add(this.picBall);
+            this.pnlMain.Controls.Add(this.btnStart);
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMain.Location = new System.Drawing.Point(0, 0);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(1144, 564);
+            this.pnlMain.TabIndex = 0;
+            this.pnlMain.Click += new System.EventHandler(this.pnlMain_Click);
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.BackColor = System.Drawing.Color.Black;
+            this.lblScore.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblScore.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblScore.Location = new System.Drawing.Point(1063, 34);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(47, 13);
+            this.lblScore.TabIndex = 7;
+            this.lblScore.Text = "Score: 0";
+            // 
+            // lblStart
+            // 
+            this.lblStart.AutoSize = true;
+            this.lblStart.BackColor = System.Drawing.Color.Transparent;
+            this.lblStart.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblStart.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblStart.Location = new System.Drawing.Point(449, 296);
+            this.lblStart.Name = "lblStart";
+            this.lblStart.Size = new System.Drawing.Size(274, 30);
+            this.lblStart.TabIndex = 5;
+            this.lblStart.Text = "Press SPACE button to start!";
+            this.lblStart.Visible = false;
+            // 
+            // lblGameOver
+            // 
+            this.lblGameOver.AutoSize = true;
+            this.lblGameOver.BackColor = System.Drawing.Color.Transparent;
+            this.lblGameOver.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblGameOver.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblGameOver.Location = new System.Drawing.Point(479, 246);
+            this.lblGameOver.Name = "lblGameOver";
+            this.lblGameOver.Size = new System.Drawing.Size(216, 50);
+            this.lblGameOver.TabIndex = 2;
+            this.lblGameOver.Text = "Game Over!";
+            this.lblGameOver.Visible = false;
+            // 
+            // picGameOver
+            // 
+            this.picGameOver.Image = ((System.Drawing.Image)(resources.GetObject("picGameOver.Image")));
+            this.picGameOver.Location = new System.Drawing.Point(387, 87);
+            this.picGameOver.Name = "picGameOver";
+            this.picGameOver.Size = new System.Drawing.Size(398, 274);
+            this.picGameOver.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picGameOver.TabIndex = 6;
+            this.picGameOver.TabStop = false;
+            this.picGameOver.Visible = false;
+            // 
+            // picPaddle
+            // 
+            this.picPaddle.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.picPaddle.Image = ((System.Drawing.Image)(resources.GetObject("picPaddle.Image")));
+            this.picPaddle.Location = new System.Drawing.Point(488, 540);
+            this.picPaddle.Name = "picPaddle";
+            this.picPaddle.Size = new System.Drawing.Size(180, 14);
+            this.picPaddle.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picPaddle.TabIndex = 3;
+            this.picPaddle.TabStop = false;
+            // 
+            // lblBall
+            // 
+            this.lblBall.AutoSize = true;
+            this.lblBall.BackColor = System.Drawing.Color.Black;
+            this.lblBall.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblBall.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblBall.Location = new System.Drawing.Point(1063, 21);
+            this.lblBall.Name = "lblBall";
+            this.lblBall.Size = new System.Drawing.Size(67, 13);
+            this.lblBall.TabIndex = 3;
+            this.lblBall.Text = "Lost balls: 0";
+            // 
+            // lblBounce
+            // 
+            this.lblBounce.AutoSize = true;
+            this.lblBounce.BackColor = System.Drawing.Color.Black;
+            this.lblBounce.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblBounce.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblBounce.Location = new System.Drawing.Point(1063, 8);
+            this.lblBounce.Name = "lblBounce";
+            this.lblBounce.Size = new System.Drawing.Size(58, 13);
+            this.lblBounce.TabIndex = 2;
+            this.lblBounce.Text = "Bounce: 0";
+            // 
+            // picBall
+            // 
+            this.picBall.BackColor = System.Drawing.Color.Transparent;
+            this.picBall.Image = ((System.Drawing.Image)(resources.GetObject("picBall.Image")));
+            this.picBall.Location = new System.Drawing.Point(563, 127);
+            this.picBall.Name = "picBall";
+            this.picBall.Size = new System.Drawing.Size(25, 25);
+            this.picBall.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBall.TabIndex = 1;
+            this.picBall.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(1141, 51);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(652, 107);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1144, 564);
+            this.Controls.Add(this.pnlMain);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.pnlMain.ResumeLayout(false);
+            this.pnlMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picGameOver)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPaddle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlMain;
+        private System.Windows.Forms.PictureBox picBall;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblBounce;
+        private System.Windows.Forms.Label lblBall;
+        private System.Windows.Forms.Label lblGameOver;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox picPaddle;
+        private System.Windows.Forms.Label lblStart;
+        private System.Windows.Forms.PictureBox picGameOver;
+        private System.Windows.Forms.Label lblScore;
+    }
+}
+
---
+++ PaddleWar/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PaddleWar
+{
+    public partial class frmMain : Form
+    {
+        Timer tmrBallMove;
+        Timer tmrLeft;
+        Timer tmrRight;
+        double bounce = 0;
+        double bounceTotal = 0;
+        double balls = 0;
+        double score = 0;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private int step = 5;
+        private int angle = 0;
+        public enum Way { Up = 1, Down = 2, Left = 3, Right = 4};
+        Way currentWay;
+       
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            StartGame();
+        }
+
+        private void StartGame()
+        {
+            lblGameOver.Visible = false;
+            lblStart.Visible = false;
+            picGameOver.Visible = false;
+
+            bounce = 0;
+
+            picBall.Location = new Point(pnlMain.Size.Width / 2, pnlMain.Size.Height / 2);
+
+            currentWay = 0;
+            tmrBallMove = new Timer();
+            tmrBallMove.Interval = 1;
+            tmrBallMove.Tick += tmrBallMove_Tick;
+            tmrBallMove.Start();
+
+            tmrLeft = new Timer();
+            tmrLeft.Interval = 50;
+            tmrLeft.Tick += tmrLeft_Tick;
+
+            tmrRight = new Timer();
+            tmrRight.Interval = 50;
+            tmrRight.Tick += tmrRight_Tick;
+
+            textBox1.Focus();
+        }
+
+        void tmrRight_Tick(object sender, EventArgs e)
+        {
+            if (tmrRight.Interval > 20)
+                tmrRight.Interval -= 10;
+            MoveRight();
+        }
+
+        void tmrLeft_Tick(object sender, EventArgs e)
+        {
+            if (tmrLeft.Interval > 20)
+                tmrLeft.Interval -= 10;
+            MoveLeft();
+        }
+
+        void tmrBallMove_Tick(object sender, EventArgs e)
+        {
+            int ballDown = picBall.Location.Y + picBall.Size.Height;
+            int ballUp = picBall.Location.Y;
+            int ballLeft = picBall.Location.X;
+            int ballRight = picBall.Location.X + picBall.Size.Width;
+
+            int edgeRight = pnlMain.Size.Width;
+            int edgeLeft = 0;
+            int edgeUp = 0;
+            int edgeDown =  pnlMain.Size.Height;
+
+            int paddleLeft = picPaddle.Location.X;
+            int paddleRight = picPaddle.Location.X + picPaddle.Size.Width;
+            int paddleUp = picPaddle.Location.Y;
+
+            if (currentWay == 0)
+                currentWay = Way.Down;
+
+            if (currentWay == Way.Down)
+            {
+                if (ballLeft <= edgeLeft)
+                    angle = -angle;
+
+                if (ballRight >= edgeRight)
+                    angle = -angle;
+
+                if (ballDown < paddleUp)
+                {
+                    picBall.Location = new Point(picBall.Location.X + angle, picBall.Location.Y + 5);
+                    currentWay = Way.Down;
+                }
+                else
+                    if (ballRight >= paddleLeft && ballLeft <= paddleRight) //odraz od pádla
+                    {
+                        Random rnd = new Random();
+                        currentWay = Way.Up;
+                        angle = 1 + rnd.Next(5);
+                        bounce++;
+                        bounceTotal++;
+                        lblBounce.Text = "Bounce: " + bounce.ToString();
+                        
+                    }
+                    else
+                    {
+                        picBall.Location = new Point(picBall.Location.X + angle, picBall.Location.Y + 5);
+                        currentWay = Way.Down;
+                    }
+
+                if (ballUp >= edgeDown)
+                {
+                    tmrBallMove.Stop();
+                    //lblGameOver.Visible = true;
+                    lblStart.Visible = true;
+                    picGameOver.Visible = true;
+                    balls++;
+                    lblBall.Text = "Lost Balls: " + balls.ToString();
+                }
+            }
+
+            if (currentWay == Way.Up)
+            {
+                if (ballLeft <= edgeLeft)
+                    angle = -angle;
+
+                if (ballRight >= edgeRight)
+                    angle = -angle;
+
+                if (ballUp > edgeUp)
+                {
+                    picBall.Location = new Point(picBall.Location.X - angle, picBall.Location.Y - 5);
+                    currentWay = Way.Up;
+                }
+                else
+                {
+                    Random rnd = new Random();
+                    currentWay = Way.Down;
+                    angle = -(angle) + (5- rnd.Next(5));
+                }
+            }
+
+            if (balls > 0)
+                score = Math.Round(bounceTotal / balls,1);
+            else
+                score = bounce;
+            lblScore.Text = "Score: " + score;
+
+        }
+
+        private void MoveLeft()
+        {
+            if (picPaddle.Location.X >= 0) 
+            picPaddle.Location = new Point(picPaddle.Location.X - (step+5), picPaddle.Location.Y);
+        }
+
+        private void MoveRight()
+        {
+            if (picPaddle.Location.X + picPaddle.Size.Width  <= pnlMain.Size.Width) 
+            picPaddle.Location = new Point(picPaddle.Location.X + (step +5), picPaddle.Location.Y);
+        }
+       
+      
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+                tmrLeft.Enabled = true;
+
+            else if (e.KeyCode == Keys.Right)
+                tmrRight.Enabled = true;
+            else if (e.KeyCode == Keys.Space)
+                if (picGameOver.Visible)
+                    StartGame();
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            tmrLeft.Enabled = false;
+            tmrRight.Enabled = false;
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            btnStart.Focus();
+            lblStart.Visible = true;
+           
+        }
+
+      
+
+        private void pnlMain_Click(object sender, EventArgs e)
+        {
+            textBox1.Focus();
+        }
+    }
+}
